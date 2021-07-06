54d3c4eb1f4cb3d143e7747e45b02d1facbce21e
Sam Hollyer <rsamhollyer@gmail.com>
2021-07-06 13:19:21 -0500

    add prisma schema

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 const express = require('express');
 const morgan = require('morgan');
 const es6Renderer = require('express-es6-template-engine');
+
 const app = express();
 const server = http.createServer(app);
 const PORT = 3000;
@@ -14,8 +15,9 @@ app.set('views', './views');
 app.set('view engine', 'html');
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(logger);
-server.listen(PORT, HOST, () => {console.log(`Listening at http://${HOST}:${PORT}`)})
-
+server.listen(PORT, HOST, () => {
+  console.log(`Listening at http://${HOST}:${PORT}`);
+});
