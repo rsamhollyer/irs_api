d6ecc1465b931f3d0d5b01b7dfa0d1bcb479a123
Sam Hollyer <rsamhollyer@gmail.com>
2021-07-07 13:00:46 -0500

    update scheme. add more to scrap logic

--- api/scrape.js
+++ api/scrape.js
@@ -1,22 +1,50 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-async function getData() {
-  try {
-    const { data } = await axios.get(
-      `https://apps.irs.gov/app/picklist/list/priorFormPublication.html?sortColumn=sortOrder&indexOfFirstRow=0&value=&criteria=&resultsPerPage=200&isDescending=false`
-    );
-    const $ = cheerio.load(data);
+function searializeForms(dataObject) {
+  const $ = cheerio.load(dataObject);
+  const formArray = [];
+  const instructionArray = [];
 
-    // console.log(
-    //   $(`.picklist-dataTable`)
-    //     .find(`.${'even' || 'odd'}`)
-    //     .first()
-    //     .text()
-    // );
+  $('.picklist-dataTable')
+    .find('tr')
+    .each((i, element) => {
+      if ($(element).attr('class') !== undefined) {
+        const product = $(element).find('a').text();
+        const title = $(element).find($('.MiddleCellSpacer')).text().trim();
+        const revision_date = $(element)
+          .find($('.EndCellSpacer'))
+          .text()
+          .trim();
+        const url = $(element).find('a').attr('href');
+        if (!product.startsWith('Inst')) {
+          formArray[i] = { product, title, revision_date, url };
+        } else {
+          instructionArray.push({ product, title, revision_date, url });
+        }
+      }
+    });
+  console.log(formArray, instructionArray);
+}
+
+async function getData(
+  params = {
+    sortColumn: 'sortOrder',
+    indexOfFirstRow: 0,
+    value: '',
+    criteria: '',
+    resultsPerPage: 200,
+    isDescending: false,
+  }
+) {
+  const url = `https://apps.irs.gov/app/picklist/list/priorFormPublication.html`;
+
+  try {
+    const { data } = await axios.get(url, { params });
+
+    searializeForms(data);
   } catch (error) {
     console.log(error);
   }
 }
-
 getData();
